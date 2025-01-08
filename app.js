341a84721b0811a8630367602724599756435c98
blogger84039 <yichuimaoye28@gmail.com>
2025-01-08 19:29:32 +0900

    feat: ユーザーにしっかりと名言を読んでいただいたうえで入力していただくため一定時間経過するまで入力できない仕組みを作成した

--- app.js
+++ app.js
@@ -2,34 +2,36 @@
 
 const wordList = [
     {
-    word: '苦しいから逃げるのではない。逃げるから苦しくなるのだ。',
-    person: 'ウィリアム・ジェームズ（心理学者）'
+        word: '苦しいから逃げるのではない。逃げるから苦しくなるのだ。',
+        person: 'ウィリアム・ジェームズ（心理学者）'
     },
     {
-    word: 'あなたは挑戦しては失敗する。 挑戦しては失敗する。挑戦しては失敗する、を繰り返す。しかし本当の失敗は、あなたが 挑戦することを辞めることだ。',
-    person: 'ウォルト・ディズニー（アニメーター、ディズニー創立者）'
+        word: 'あなたは挑戦しては失敗する。 挑戦しては失敗する。挑戦しては失敗する、を繰り返す。しかし本当の失敗は、あなたが 挑戦することを辞めることだ。',
+        person: 'ウォルト・ディズニー（アニメーター、ディズニー創立者）'
     },
     {
-    word: '才能とは継続する情熱のことである。',
-    person: 'モーパッサン（作家、劇作家）'
+        word: '才能とは継続する情熱のことである。',
+        person: 'モーパッサン（作家、劇作家）'
     },
     {
-    word: 'インスピレーションを待っていたら 何もかけない。私は毎朝必ず作曲をする。そうすると神様がインスピレーションを 送ってくださるんだ。',
-    person: 'チャイコフスキー（作曲家）'
+        word: 'インスピレーションを待っていたら 何もかけない。私は毎朝必ず作曲をする。そうすると神様がインスピレーションを 送ってくださるんだ。',
+        person: 'チャイコフスキー（作曲家）'
     },
     {
-    word: 'あなたができると思えばできる。できないと思えばできない。',
-    person: 'ヘンリー・フォード（フォード・モーター創設者）'
+        word: 'あなたができると思えばできる。できないと思えばできない。',
+        person: 'ヘンリー・フォード（フォード・モーター創設者）'
     },
     {
-    word: '何事も80%ぐらいが一番ちょうどいい',
-    person: 'オリジナル'
+        word: '何事も80%ぐらいが一番ちょうどいい',
+        person: 'オリジナル'
     }
 ];
 
 const wordBox = document.getElementById('wordBox');
 const word = document.getElementById('word');
 const person = document.getElementById('person');
+const input = document.getElementById('input');
+const button = document.getElementById('button');
 
 let random = 0;
 let Done = [];
@@ -43,3 +45,15 @@ randomFunc();
 
 word.textContent = wordList[random].word;
 person.textContent += wordList[random].person;
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (random === 1 || random === 3) {
+        setTimeout(() => {
+            input.disabled = false;
+        }, 8000);
+    } else {
+        setTimeout(() => {
+            input.disabled = false;
+        }, 5000);
+    }
+})
